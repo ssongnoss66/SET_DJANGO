441c889a4deef91c622e11e3933da2a70777ceb0
“SSONGNOSS66” <ssongnoss66@gmail.com>
2023-03-21 16:30:26 +0900

    ADD 0321 AS

--- firstpjt/urls.py
+++ firstpjt/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from berners_lee import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('berners_lee/', views.berners_lee),
 ]
