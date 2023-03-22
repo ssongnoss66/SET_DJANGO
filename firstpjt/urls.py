4f15620d096af090e12a9cd9cc4ba196fadd7681
“SSONGNOSS66” <ssongnoss66@gmail.com>
2023-03-22 23:14:23 +0900

    0322

--- firstpjt/urls.py
+++ firstpjt/urls.py
@@ -20,4 +20,9 @@ from berners_lee import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('berners_lee/', views.berners_lee),
+    path('index/', views.index),
+    path('dinner/', views.dinner),
+    path('search/', views.search),
+    path('throw/', views.throw),
+    path('catch/', views.catch),
 ]
